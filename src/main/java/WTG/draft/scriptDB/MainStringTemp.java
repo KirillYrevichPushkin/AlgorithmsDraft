5f4ca48f65e3c14a3e1b559ae83f714049a4c811
KirillPushkin <benberov09@mail.ru>
2023-02-01 16:26:24 +0300

    01.02.2023

--- src/main/java/WTG/draft/scriptDB/MainStringTemp.java
+++ src/main/java/WTG/draft/scriptDB/MainStringTemp.java
@@ -3,6 +3,8 @@ package WTG.draft.scriptDB;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class MainStringTemp {
@@ -17,7 +19,9 @@ public class MainStringTemp {
 //                "\"GeocoderMetaData\":{\"precision\":\"exact\",\"text\":\"Россия, Краснодар, Рашпилевская улица, 130/1\"," +
 //                "\"kind\":\"house\"}}}]}\n");
 
-        String str = new String("Витаминкомбинат,_Краснодарский_край,_Россия,_350031&type=geo&lang=ru_RU&apikey=56a66ddf-edfc-40a0-b66e-f59f7826e136\n" +
+        String str = new String("{\"type\":\"FeatureCollection\",\"properties\":{\"ResponseMetaData\":{\"SearchResponse\":{\"found\":3,\"display\":\"single\",\"boundedBy\":[[38.958515,45.022093],[38.966725,45.027916]]},\"SearchRequest\":{\"request\":\"ленина_14_краснодар\",\"skip\":0,\"results\":10,\"boundedBy\":[[37.048427,55.43644866],[38.175903,56.04690174]]}}},\"features\":[{\"type\":\"Feature\",\"geometry\":{\"type\":\"Point\",\"coordinates\":[38.96262,45.025004]},\"properties\":{\"name\":\"улица Ленина, 14\",\"description\":\"микрорайон Центральный, Центральный внутригородской округ, Краснодар, Россия\",\"boundedBy\":[[38.958515,45.022093],[38.966725,45.027916]],\"GeocoderMetaData\":{\"precision\":\"exact\",\"text\":\"Россия, Краснодар, Центральный внутригородской округ, микрорайон Центральный, улица Ленина, 14\",\"kind\":\"house\"}}},{\"type\":\"Feature\",\"geometry\":{\"type\":\"Point\",\"coordinates\":[39.078898,45.02201]},\"properties\":{\"name\":\"улица Ленина, 14\",\"description\":\"жилой массив Пашковский, Карасунский внутригородской округ, Краснодар, Россия\",\"boundedBy\":[[39.074792,45.019099],[39.083003,45.024922]],\"GeocoderMetaData\":{\"precision\":\"exact\",\"text\":\"Россия, Краснодар, Карасунский внутригородской округ, жилой массив Пашковский, улица Ленина, 14\",\"kind\":\"house\"}}},{\"type\":\"Feature\",\"geometry\":{\"type\":\"Point\",\"coordinates\":[39.215433,45.028941]},\"properties\":{\"name\":\"хутор Ленина, 4\",\"description\":\"муниципальное образование Краснодар, Россия\",\"boundedBy\":[[39.211327,45.02603],[39.219538,45.031852]],\"GeocoderMetaData\":{\"precision\":\"near\",\"text\":\"Россия, муниципальное образование Краснодар, хутор Ленина, 4\",\"kind\":\"house\"}}}]}\n");
+
+        String str1 = new String("Витаминкомбинат,_Краснодарский_край,_Россия,_350031&type=geo&lang=ru_RU&apikey=56a66ddf-edfc-40a0-b66e-f59f7826e136\n" +
                 "{\"type\":\"FeatureCollection\",\"properties\":{\"ResponseMetaData\":{\"SearchResponse\":" +
                 "{\"found\":7,\"display\":\"single\",\"boundedBy\":[[38.984575,45.142962],[39.003529,45.15298]]}," +
                 "\"SearchRequest\":{\"request\":\"Tselinogradskaya_Ulitsa,_32,_Витаминкомбинат,_Краснодарский_край,_Россия,_350031\"," +
@@ -58,8 +62,11 @@ public class MainStringTemp {
 
         System.out.println(str);
 
+        long l1 = System.currentTimeMillis();
         List<Double> coordinateList = new ArrayList<>();
+      //  coordinateList =  Arrays.stream((str.substring(str.indexOf("coordinate") +14, str.lastIndexOf("]},", str.indexOf("coordinate") +14 + 21)))
         coordinateList =  Arrays.stream((str.substring(str.indexOf("coordinate") +14, str.lastIndexOf("]},", str.indexOf("coordinate") +14 + 21)))
+
                 .split(","))
                 .map( s -> {
                     String st = s.replace("]","0");
@@ -72,7 +79,37 @@ public class MainStringTemp {
 
         System.out.println("indexOf " + str.indexOf("coordinate"));
         System.out.println("lastIndexOf " + str.lastIndexOf("]},"));
+        long l2 = System.currentTimeMillis();
+        System.out.println("Time1 = " + (l2-l1));
 
+
+        long l3 = System.currentTimeMillis();
+        System.out.println("with regex");
+        Pattern pattern = Pattern.compile("(\\\"coordinates\\\")([:][\\[])([0-9.,]*)([\\]][\\}])");
+        Matcher matcher = pattern.matcher(str1);
+        System.out.println(matcher.find());
+
+        String coordinate = matcher.group();
+        System.out.println(coordinate);
+
+        List<Double> coordinateList2 = new ArrayList<>();
+
+        Pattern pattern1 = Pattern.compile("[0-9.]+");
+        Matcher matcher1 = pattern1.matcher(coordinate);
+
+        System.out.println(matcher1.find());
+        System.out.println(" match1 = " + matcher1.group());
+
+        coordinateList2.add(Double.valueOf( matcher1.group()));
+
+        System.out.println(matcher1.find());
+        System.out.println(" match2 = " + matcher1.group());
+        long l4 = System.currentTimeMillis();
+
+        coordinateList2.add(Double.valueOf( matcher1.group()));
+
+        System.out.println(coordinateList2.toString());
+        System.out.println("Time2 = " + (l4-l3));
 
 
     }
