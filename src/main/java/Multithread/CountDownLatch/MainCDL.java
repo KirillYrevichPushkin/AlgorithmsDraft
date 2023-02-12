24e8802f3f9e288249bf4d26f96b970753c212be
KirillPushkin <benberov09@mail.ru>
2023-02-12 15:24:12 +0300

    12.02.2023 CountDownLatch

---
+++ src/main/java/Multithread/CountDownLatch/MainCDL.java
@@ -0,0 +1,45 @@
+package Multithread.CountDownLatch;
+
+import java.util.concurrent.CountDownLatch;
+
+public class MainCDL {
+    public static void main(String[] args) {
+        CountDownLatch countDownLatch = new CountDownLatch(5);
+        System.out.println(Thread.currentThread().getName() + " start");
+
+        new Thread(new CDLTemp(countDownLatch)).start();
+
+
+        try {
+            countDownLatch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println(Thread.currentThread().getName() + " end");
+
+    }
+
+
+}
+
+class CDLTemp implements Runnable{
+    CountDownLatch countDownLatch;
+
+    public CDLTemp(CountDownLatch countDownLatch) {
+        this.countDownLatch = countDownLatch;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 7; i++) {
+            try {
+                Thread.sleep(200);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            System.out.println("countDownLatch.countDown();" + countDownLatch.getCount());
+            countDownLatch.countDown();
+        }
+    }
+}
