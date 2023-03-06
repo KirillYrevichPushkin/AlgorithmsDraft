1f6f68872f9ee585df2a3871376419d2b8331836
KirillPushkin <benberov09@mail.ru>
2023-03-06 12:23:22 +0300

    06.03.2023

--- src/main/java/TestTask/BaltInfocom/MainBalt3.java
+++ src/main/java/TestTask/BaltInfocom/MainBalt3.java
@@ -7,18 +7,18 @@ import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class MainBalt3 {
-//    static Pattern pattern1 = Pattern.compile("\"\\d{1,10}\"");
-//    static Pattern pattern2 = Pattern.compile("\"\\d{12,}\"");
-//    static Matcher matcher1;
-//    static Matcher matcher2;
+    static Pattern pattern1 = Pattern.compile("\"\\d{1,10}\"");
+    static Pattern pattern2 = Pattern.compile("\"\\d{12,}\"");
+    static Matcher matcher1;
+    static Matcher matcher2;
+
+    static int numberGroup = 0;
 
     public static void main(String[] args) {
-        File file = new File("D:\\Download\\lngmaxtest2.txt");
+        File file = new File("D:\\Download\\lng.txt");
         Set<String> stringSet = new TreeSet<>();
         List<Row> rows = new ArrayList<>(300);
-        //Map<String, List<Row>> stringListMap = new HashMap<>(6000000);
-        Map<String, List<Row>> stringListMap = new HashMap<>(6000000);
-        int maxSize = 0;
+        Map<String, List<Row>> stringListMap = new HashMap<>();
 
         long t1 = System.currentTimeMillis();
         try(BufferedReader br = new BufferedReader(new FileReader(file))) {
@@ -26,12 +26,13 @@ public class MainBalt3 {
             String line = br.readLine();
             while (line != null) {
              //   System.out.println(line);
-//                matcher1 = pattern1.matcher(line);
-//                matcher2 = pattern2.matcher(line);
-//                if(matcher1.find() || matcher2.find()){
-//                    System.out.println(line);
-//                    continue;
-//                }
+                matcher1 = pattern1.matcher(line);
+                matcher2 = pattern2.matcher(line);
+                if(matcher1.find() || matcher2.find()){
+                    System.out.println(line);
+                    line = br.readLine();
+                    continue;
+                }
                 stringSet.add(line);
                 line = br.readLine();
             }
@@ -50,13 +51,13 @@ public class MainBalt3 {
 
         String[] tempString;
         Row tempRow;
-        int numberGroup = 0;
+
         Iterator<String> stringIterator = stringSet.iterator();
         long t3 = System.currentTimeMillis();
         while (stringIterator.hasNext()) {
             tempString = stringIterator.next().split(";");
             tempRow = new Row(tempString);
-            if(tempString.length > maxSize) maxSize = tempString.length;
+
             rows.add(tempRow);
             for (int i = 0; i < tempString.length; i++) {
                 List<Row> rowList;
@@ -78,46 +79,9 @@ public class MainBalt3 {
             if(me.getValue().size()==1){
                 continue;
             }else {
-                int maxLength = 0;
-                Row tempRowMap;
-                for (int i = 0; i < me.getValue().size(); i++) {
-                    tempRowMap = me.getValue().get(i);
-                    if(tempRowMap.getStringRow().length > maxLength) maxLength = tempRowMap.getStringRow().length;
-                }
-                int[] arrIndexKey = new int[me.getValue().size()];
-                for (int i = 0; i < me.getValue().size(); i++) {
-                    tempRowMap = me.getValue().get(i);
-                    for (int j = 0; j < tempRowMap.getStringRow().length; j++) {
-                        if(tempRowMap.getStringRow()[j].equals(me.getKey())) {
-                            arrIndexKey[i] = j;
-                            break;
-                        }
-                    }
-                }
-                int sizeGroup = 0;
-                for (int i = 0; i < arrIndexKey.length; i++) {
-                    for (int j = i+1; j <arrIndexKey.length ; j++) {
-                        if(arrIndexKey[i] == arrIndexKey[j]){
-                            if(me.getValue().get(i).getGroupNumber()==-1 && me.getValue().get(j).getGroupNumber()==-1) {
-                                me.getValue().get(i).setGroupNumber(numberGroup);
-                                me.getValue().get(j).setGroupNumber(numberGroup);
-                                numberGroup++;
-                                sizeGroup = 2;
-                            }else if(me.getValue().get(i).getGroupNumber()==-1){
-                            me.getValue().get(i).setGroupNumber(me.getValue().get(j).getGroupNumber());
-                                sizeGroup++;
-                            }else if(me.getValue().get(j).getGroupNumber()==-1){
-                            me.getValue().get(j).setGroupNumber(me.getValue().get(i).getGroupNumber());
-                                sizeGroup++;
-                            }
 
-                        }
-                    }
-                }
+                setNumberGroup(me);
 
-                for (int i = 0; i < arrIndexKey.length; i++) {
-                    me.getValue().get(i).setSizeGroup(sizeGroup);
-                }
             }
         }
 
@@ -127,15 +91,41 @@ public class MainBalt3 {
                 .sorted(new Comparator<Row>() {
                     @Override
                     public int compare(Row o1, Row o2) {
-                        return o2.getSizeGroup()-o1.getSizeGroup();
+                        return o1.getSizeGroup()-o2.getSizeGroup();
                     }
                 })
                 .collect(Collectors.groupingBy(Row::getSizeGroup));
 
 
-        for (Map.Entry<Integer, List<Row>> merg: rowGroups.entrySet() ) {
-            if(merg.getValue().size()>1) {
-                System.out.println("Count Group = " + merg.getKey());
+        TreeMap<Integer, List<Row>> treeMapRows = new TreeMap<>(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o2-o1;
+            }
+        });
+
+        for (Map.Entry<Integer, List<Row>> me: rowGroups.entrySet()) {
+
+            treeMapRows.put(me.getKey(), me.getValue().stream().sorted(new Comparator<Row>() {
+                @Override
+                public int compare(Row o1, Row o2) {
+                    return o1.getGroupNumber()-o2.getGroupNumber();
+                }
+            }).collect(Collectors.toList()));
+        }
+
+
+
+//        for (Map.Entry<Integer, List<Row>> merg: rowGroups.entrySet() ) {
+//            if(merg.getValue().size()>1) {
+//                System.out.println("Count Group = " + merg.getKey());
+//                merg.getValue().stream().forEach(System.out::println);
+//            }
+//        }
+
+        for (Map.Entry<Integer, List<Row>> merg: treeMapRows.entrySet() ) {
+            if(merg.getKey()>=2) {
+                System.out.println("Size Group = " + merg.getKey());
                 merg.getValue().stream().forEach(System.out::println);
             }
         }
@@ -156,6 +146,58 @@ public class MainBalt3 {
 //        System.out.println("количество групп с более 1 элемента = " + sortedListRow.size());
 
     }
+
+
+
+    public static void  setNumberGroup(Map.Entry<String, List<Row>> me){
+        List<Row> rowList = me.getValue().stream().filter(r-> r.getGroupNumber()==-1).collect(Collectors.toList());
+        if(rowList.size()<2){
+            return;
+        }
+
+        int[] arrIndexKey = new int[rowList.size()];
+
+        for (int i = 0; i < rowList.size(); i++) {
+            for (int j = 0; j < rowList.get(i).getStringRow().length; j++) {
+                if(rowList.get(i).getStringRow()[j].equals(me.getKey())) {
+                    arrIndexKey[i] = j;
+                    break;
+                }
+            }
+        }
+
+        int sizeGroup = 0;
+
+            for (int i = 0; i < arrIndexKey.length; i++) {
+                for (int j = i+1; j < arrIndexKey.length ; j++) {
+                    if(arrIndexKey[i] == arrIndexKey[j]){
+                        if(rowList.get(i).getGroupNumber()!=-1 && rowList.get(j).getGroupNumber()!=-1){
+                            continue;
+                        }
+                        if(rowList.get(i).getGroupNumber()==-1 && rowList.get(j).getGroupNumber()==-1) {
+                            rowList.get(i).setGroupNumber(numberGroup);
+                            rowList.get(j).setGroupNumber(numberGroup);
+                            numberGroup++;
+                            sizeGroup = 2;
+                        }else if(rowList.get(i).getGroupNumber()==-1 && rowList.get(j).getGroupNumber()!=-1){
+                            rowList.get(i).setGroupNumber(rowList.get(j).getGroupNumber());
+                            sizeGroup++;
+                        }else if(rowList.get(j).getGroupNumber()==-1 && rowList.get(i).getGroupNumber()!=-1){
+                            rowList.get(j).setGroupNumber(rowList.get(i).getGroupNumber());
+                            sizeGroup++;
+                        }
+
+                    }
+                }
+            }
+
+            for (int i = 0; i < arrIndexKey.length; i++) {
+               if(rowList.get(i).getGroupNumber()!=-1) {
+                   rowList.get(i).setSizeGroup(sizeGroup);
+               }
+            }
+    }
+
 
 }
 
